761e785fc0acaa65bae2e2b9aea1b9876072e1e2
Alison Marques <alison-marques@outlook.com.br>
2020-07-01 15:38:04 -0300

    Controle de paginaçao de agendamentos

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -9,9 +9,13 @@ class AppointmentController {
    * listando todos os agendamentos do usuário logado
    */
   async index(req, res) {
+    const { page = 1 } = req.query;
+
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       attributes: ['id', 'date'],
       include: [
         {
