9038c0ed56e5e6fc6770178c33287ce18be33b2f
Alison Marques <alison-marques@outlook.com.br>
2020-07-02 16:19:26 -0300

    Email para notificar o cancelamento do agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,6 +6,8 @@ import File from '../models/File';
 import Notification from '../schemas/Notification';
 import * as Yup from 'yup';
 
+import Mail from '../../lib/Mail';
+
 class AppointmentController {
   /**
    * listando todos os agendamentos do usuário logado
@@ -124,7 +126,15 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
 
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
@@ -143,6 +153,12 @@ class AppointmentController {
     appointment.canceled_at = new Date();
 
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      text: 'Você tem um novo cancelamento',
+    });
 
     return res.json(appointment);
   }
