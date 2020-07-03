d33d9b8855b1305807b92061ff3c7a00bacd7fb5
Alison Marques <alison-marques@outlook.com.br>
2020-07-03 14:56:51 -0300

    Serve para mostrar mais informaçoes do agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -20,9 +20,9 @@ class AppointmentController {
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       limit: 20,
       offset: (page - 1) * 20,
-      attributes: ['id', 'date'],
       include: [
         {
           model: User,
